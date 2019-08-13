632af8e038ad53eb273d038af5937ec61d59a5c7
mariana fernandez garcia <marianafernandezgarcia@gmail.com>
2019-08-13 15:06:31 +0200

    hoja 1

--- app/views.py
+++ app/views.py
@@ -1,15 +1,46 @@
 from app import app
-from flask import render_template
+from flask import render_template, request ,redirect, url_for, flash
+import csv 
 
-@app.route('/')
+ficheroPelicula = 'data/pelicula.txt'
+
+@app.route('/', methods= ['GET','POST'])
 def index():
-    return render_template('index.html')
+    if request.methods == 'POST':
+        Fpelicula = open(ficheroPelicula,'a+')
+        registro = (request.values['comentarios'])
+        #lo tomo de index? el value? o es otra cosa
+        Fpelicula.write(registro)
+        Fpelicula.close()
+        return redirect (ulr_for('indice'))
+    else:
+        msg = validar (request.values)
+        if msg != True:
+            return render_template('/', errores = msg)
 
-@app.route('/')
+ def validar (values):
+    errores = []
+    if values ['comentarios']=='':
+        error.append('debe introduces un titulo')
+    if len(errores) == 0:
+        return True
+    else:
+        return errores
+
+
+@app.route('/indice',methods= ['GET','POST'])
 def indice():
+    if request.methods == 'GET':
+        Fpelicula= open(ficheroPelicula,'r')
+        
+    ##lo traigo relleno de arriba y debo leer el titutlo para usarlo
+    ##comparar los datos y enviar la elegida 
+
     return render_template('indice.html')
 
-@app.route('/')
+@app.route('/final')
 def final():
     return render_template('final.html')
+
+
     
