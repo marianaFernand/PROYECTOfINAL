71e6972efd48f55150f55a25ac643dc7235cd42f
mariana fernandez garcia <marianafernandezgarcia@gmail.com>
2019-08-23 13:15:26 +0200

    templates borrador

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 [omdb]
 SECRET_KEY= B'daa89334'
-ALL_FILM = http://www.omdbapi.com/?i=tt3896198&apikey={}
-CHECK_FILM = http://www.omdbapi.com/?apikey{}&t={}
+ALL_FILM = http://www.omdbapi.com/?apikey={}&i={}
+CHECK_FILM = http://www.omdbapi.com/?apikey={}&t={}
